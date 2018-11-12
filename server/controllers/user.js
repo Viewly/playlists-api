e59a23248292d389356daecc61ff097feb29ecd6
denkomanceski <denkomanceski@gmail.com>
2018-11-12 14:30:34 +0100

    Add validators to user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,16 +3,16 @@ const users = require('../domain/user/index');
 const helpers = require('../utils/helpers');
 const bookmarks = require('../domain/bookmarks/index');
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
+const userValidators = require('../validators/user');
 
 
-router.post('/register', (req, res) => {
+router.post('/register', userValidators.registerUser, (req, res) => {
   users.registerUser(req.body).then(data => {
     res.json(data);
   }).catch(err => res.json(err))
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', userValidators.loginUser, (req, res) => {
   users.loginUser(req.body.email, req.body.password).then(data => {
     res.json(data);
   }).catch(err => res.json(err))
@@ -23,14 +23,14 @@ router.get('/auth/:platform', (req, res, next) => passport.authenticate(req.para
   res.json(user);
 })(req, res, next));
 
-router.post('/reset-password-request', (req, res) => {
+router.post('/reset-password-request', userValidators.resetPasswordRequest, (req, res) => {
   const email = req.body.email;
   email ? users.resetPasswordRequest(email).then(data => {
       res.json(data);
     }) : res.json({success: false, reason: "Email address is required."})
 });
 
-router.post('/reset-password', (req, res) => {
+router.post('/reset-password', userValidators.resetPassword, (req, res) => {
   if (!req.body.password_reset_token) {
     res.json({success: false, reason: "Missing token."})
   } else if (!req.body.password) {
@@ -42,14 +42,7 @@ router.post('/reset-password', (req, res) => {
   }
 });
 
-router.post('/youtube-login', async (req, res) => {
-  users.registerOrLoginUserGoogle(req.body.code).then(data => {
-    data.token = jwt.sign(data.user, process.env.JWT_PASSWORD);
-    res.json(data);
-  }).catch(err => res.json(err.message))
-});
-
-router.post('/confirm-email-request', (req, res) => {
+router.post('/confirm-email-request', userValidators.confirmEmailRequest, (req, res) => {
   if (!req.body.email) res.json({ success: false, reason: "Email is missing." });
   else {
     users.sendConfirmEmailLink(req.body.email).then(data => {
@@ -58,7 +51,7 @@ router.post('/confirm-email-request', (req, res) => {
   }
 });
 
-router.post('/confirm-email', (req, res) => {
+router.post('/confirm-email', userValidators.confirmEmail, (req, res) => {
   users.confirmEmail(req.body.email_confirm_token).then(data => {
     res.json({success: true});
   }).catch(err => res.json(err))
@@ -106,8 +99,8 @@ router.get('/bookmarks', (req, res) => {
   }).catch(err => res.json(err));
 });
 
-router.post('/bookmark', (req, res) => {
-  const uuid = req.user.id;
+router.post('/bookmark', userValidators.createBookmark, (req, res) => {
+  const uuid = req.user.id || 'Viewly';
   bookmarks.createBookmark(uuid, req.body.playlist_id).then(data => {
     res.json(data);
   }).catch(err => res.json(err));
@@ -120,7 +113,7 @@ router.delete('/bookmark/:playlist_id', (req, res) => {
   }).catch(err => res.json(err));
 });
 
-router.put('/onboarding', (req, res) => {
+router.put('/onboarding', userValidators.onboarding, (req, res) => {
   const uuid = req.user.id;
   users.updateOnboarding(uuid, req.body).then(data => {
     res.json({success: true});
---
+++ server/validators/user.js
@@ -0,0 +1,51 @@
+const { check } = require('express-validator/check');
+const { validateErrors, matchData, generalValidators } = require('./index');
+
+const registerUser = [
+  generalValidators.email,
+  generalValidators.password,
+  check(['first_name', 'last_name', 'avatar_url']).trim().optional()
+];
+
+const loginUser = [
+  generalValidators.email,
+  generalValidators.password,
+];
+
+const youtubeLogin = [
+  check('code').trim().withMessage('Code is required.')
+];
+
+const resetPasswordRequest = [
+  generalValidators.email
+];
+
+const resetPassword = [
+  generalValidators.email,
+  generalValidators.password,
+  check('password_reset_token').isUUID()
+];
+
+const confirmEmailRequest = [
+  generalValidators.email
+];
+
+const confirmEmail = [
+  check('email').isEmail().trim().withMessage('Email must be a valid email address.'),
+  check('email_confirm_token').isUUID()
+];
+
+const createBookmark = [
+  check('playlist_id').isUUID()
+];
+
+const onboarding = [
+  check('categories_ids').isArray(),
+  check('time_to_spend').isInt().optional(),
+  check('step').isInt().optional()
+];
+
+const validators = {registerUser, loginUser, youtubeLogin, resetPasswordRequest, resetPassword, confirmEmailRequest, confirmEmail, createBookmark, onboarding};
+Object.keys(validators).forEach(key => { validators[key].push(validateErrors, matchData) });
+
+module.exports = validators;
