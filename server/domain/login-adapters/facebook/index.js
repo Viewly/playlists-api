9ed1d43c0b3d1f134df30b1a8702d54d5a9ac12e
denkomanceski <denkomanceski@gmail.com>
2018-12-15 13:55:51 +0100

    Facebook app ID logging

--- server/domain/login-adapters/facebook/index.js
+++ server/domain/login-adapters/facebook/index.js
@@ -6,40 +6,42 @@ const db = require('../../../../db/knex');
 const _ = require('lodash');
 
 function initializePassportStrategy() {
-  console.log("Facebook passport strategy initialized");
-  passport.use(new FacebookStrategy({
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: process.env.FACEBOOK_CALLBACK_URL || `${process.env.CURRENT_ENDPOINT}/v1/api/user/auth/facebook`,
-      profileFields: ['id', 'displayName', 'email', 'picture', 'first_name', 'last_name']
-    },
-    async (accessToken, refreshToken, profile, cb) => {
-      profile = JSON.parse(profile._raw);
-      const exists = await users.getUserByEmail(profile.email);
-      const user = {
-        facebook_access_token: accessToken,
-        facebook_refresh_token: refreshToken,
-        facebook_id: profile.id,
-        first_name: profile.first_name,
-        last_name: profile.last_name,
-        avatar_url: exists && exists.avatar_url ? exists.avatar_url : _.get(profile, 'picture.data.url', ''),
-        email: profile.email,
-        email_confirmed: true
-      };
-      let response = {};
-      if (exists && !exists.facebook_id) { //Existed but never logged with facebook
-        user.id = exists.id;
-        await updateUser(user);
-        response.message = "Thanks for linking your account with your Facebook account.";
-      } else if (!exists) { //Never existed
-        await registerUser(user);
-        response.registered = true;
+  console.log("Facebook passport strategy initialized. App ID: ", process.env.FACEBOOK_APP_ID);
+  if (process.env.FACEBOOK_APP_ID) {
+    passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK_URL || `${process.env.CURRENT_ENDPOINT}/v1/api/user/auth/facebook`,
+        profileFields: ['id', 'displayName', 'email', 'picture', 'first_name', 'last_name']
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        profile = JSON.parse(profile._raw);
+        const exists = await users.getUserByEmail(profile.email);
+        const user = {
+          facebook_access_token: accessToken,
+          facebook_refresh_token: refreshToken,
+          facebook_id: profile.id,
+          first_name: profile.first_name,
+          last_name: profile.last_name,
+          avatar_url: exists && exists.avatar_url ? exists.avatar_url : _.get(profile, 'picture.data.url', ''),
+          email: profile.email,
+          email_confirmed: true
+        };
+        let response = {};
+        if (exists && !exists.facebook_id) { //Existed but never logged with facebook
+          user.id = exists.id;
+          await updateUser(user);
+          response.message = "Thanks for linking your account with your Facebook account.";
+        } else if (!exists) { //Never existed
+          await registerUser(user);
+          response.registered = true;
+        }
+        response.user = users.getCleanUserAndJwt(await users.getUserByEmail(user.email));
+        response.success = true;
+        cb(null, response);
       }
-      response.user = users.getCleanUserAndJwt(await users.getUserByEmail(user.email));
-      response.success = true;
-      cb(null, response);
-    }
-  ));
+    ));
+  }
 }
 
 async function updateUser(user) {
