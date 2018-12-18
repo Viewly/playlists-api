420e25e194dbe02e3ad26ee7f6e06afdfa711561
denkomanceski <denkomanceski@gmail.com>
2018-12-18 20:05:10 +0100

    Fix/improve comment notifications

--- server/domain/notifications/templates.js
+++ server/domain/notifications/templates.js
@@ -25,12 +25,11 @@ async function commentedOnPlaylistAll(comment_owner, playlist_id, comment_id) {
   const playlist = (await db.select('*').
     from('playlist').
     where('id', playlist_id))[0];
-  const all_comments = await db.select('*').
+  const all_comments = await db.distinct('user_id').
     from('review').
-    where('playlist_id', playlist.id);
+    where('playlist_id', playlist.id).whereNotIn('user_id', [comment_owner, playlist.user_id]);
 
   return all_comments.map(comment => {
-      if (comment.user_id !== comment_owner && comment.user_id !== playlist.user_id) {
         return {
           template_name: 'playlist_comment_for_all',
           playlist_id: playlist.id,
@@ -41,12 +40,11 @@ async function commentedOnPlaylistAll(comment_owner, playlist_id, comment_id) {
             comment_owner,
             playlist_owner: playlist.user_id
           },
-          title: `${comment_sender.alias || comment_sender.first_name || 'Some mysterious user'} just added a comment on the playlist ${playlist.title}.`,
+          title: `${comment_sender.alias || comment_sender.first_name ||
+          'Some mysterious user'} just added a comment on the playlist ${playlist.title}.`,
           status: 'unread',
           user_id: comment.user_id
         }
-
-      }
   })
 }
 module.exports = { commentedOnPlaylist, commentedOnPlaylistAll };
