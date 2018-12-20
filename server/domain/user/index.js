cb45dae076d5c71fb03bc46e7bd19c38e4576234
denkomanceski <denkomanceski@gmail.com>
2018-12-20 11:05:51 +0100

    Confirm email fix on register

--- server/domain/user/index.js
+++ server/domain/user/index.js
@@ -39,7 +39,7 @@ async function registerUser(user){
 
         await db.insert(newUser).into('user');
         afterRegisterProcess(newUser);
-        emails.sendConfirmEmail(newUser);
+        sendConfirmEmailLink(user.email);
         return { success: true, user: getCleanUserAndJwt(newUser), registered: true };
     } else {
       return { success: false, message: "User already exists." };
