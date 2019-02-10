e243ae031d885131c5d97fd11853d6047665eb98
denkomanceski <denkomanceski@gmail.com>
2019-02-10 17:20:57 +0100

    Fix analytics endpoints

--- server/domain/playlist/analytics.js
+++ server/domain/playlist/analytics.js
@@ -4,11 +4,24 @@ const analytics_endpoint = 'https://vidflow-analytics-api.view.ly';
 async function getFeaturedPlaylists(timeframe, category_id, limit = 10) {
   let results = [];
     try {
-      results = await utils.get(`${analytics_endpoint}/high_engagement_playlists/${category_id || 'all'}/${timeframe}?limit=${limit }`);
+      let url = category_id
+        ? `${analytics_endpoint}/high_engagement_playlists/category/${category_id}/${timeframe}?limit=${limit}`
+        : `${analytics_endpoint}/high_engagement_playlists/all/${timeframe}?limit=${limit}`;
+      results = await utils.get(url);
     } catch (ex) {
       console.error(ex);
     }
     return results;
+}
+
+async function getFeaturedPlaylistsByTag(timeframe, tag, limit = 10) {
+  let results = [];
+  try {
+    results = await utils.get(`${analytics_endpoint}/high_engagement_playlists/tag/${tag}/${timeframe}?limit=${limit }`);
+  } catch (ex) {
+    console.error(ex);
+  }
+  return results;
 }
 
 async function getWatchHistory(user_id, limit = 5) {
@@ -20,4 +33,4 @@ async function getWatchHistory(user_id, limit = 5) {
   }
   return results;
 }
-module.exports = { getFeaturedPlaylists, getWatchHistory };
+module.exports = { getFeaturedPlaylists, getWatchHistory, getFeaturedPlaylistsByTag };
